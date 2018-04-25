b7c6bb88416fbb780b501acf9b98521853eb91e8
NunoMouro <32868189+NunoMouro@users.noreply.github.com>
2018-04-26 00:06:08 +0100

    Add files via upload

---
+++ VariablesDemo/VariablesDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 35; //that is true
+            int x = 5;
+            double myHourlyRate = 150.00;
+            var myName = "Nuno Mouro";
+
+            Console.WriteLine($"myAge: {myAge}, x: {x}, myHourlyRate: {myHourlyRate}, myName: {myName}");
+
+            myAge = 25; //that is true
+            x = 10;
+            myHourlyRate = 150.13;
+            myName = "Jose Hernandez";
+
+            Console.WriteLine($"myAge: {myAge}, x: {x}, myHourlyRate: {myHourlyRate}, myName: {myName}");
+        }
+    }
+}
