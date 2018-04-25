48df794b3b3c80d97ba292d685f807e209e906d1
NunoMouro <32868189+NunoMouro@users.noreply.github.com>
2018-04-25 19:28:00 +0100

    Add files via upload

---
+++ AbstractDemo/AbstractDemo/Control.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AbstractDemo
+{
+    public abstract class Control
+    {
+        protected int xPos;
+        protected int yPos;
+
+        public Control(          
+            int xPosition,
+            int yPosition)
+        {
+            xPos = xPosition;
+            yPos = yPosition;
+        }
+        
+        public virtual void Clear()
+        {
+            Console.WriteLine("Clearing control");
+        }
+
+        public abstract void Draw();
+    }
+}
---
+++ AbstractDemo/AbstractDemo/Worker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDemo
+{
+    public class Worker
+    {
+        public Worker()
+        {
+            Work();
+        }
+
+        public void Work()
+        {
+            List<Control> controls =
+                new List<Control>();
+            Button button = new Button(0, 100, "Click me");
+            Listbox listBox = new Listbox(100, 300,
+                new List<string>());
+            Button cancelButton = new Button(150, 100, "Cancel Me");
+            controls.Add(button);
+            controls.Add(listBox);
+            controls.Add(cancelButton);
+
+            foreach(Control control in controls)
+            {
+                control.Draw();
+            }
+        }
+    }
+
+    
+}
